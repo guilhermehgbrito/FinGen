186823b36179cbde2db482a11ac93d590db47384
Guilherme Brito <ghenriquegb9@gmail.com>
2021-10-07 19:44:57 -0300

    fingen_atividade: Migrations feitas e models registrados ao admin

--- fin_gen/apps/fingen_atividade/admin.py
+++ fin_gen/apps/fingen_atividade/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Atividade, Categoria, Icone
+
+admin.site.register(Atividade)
+admin.site.register(Categoria)
+admin.site.register(Icone)
