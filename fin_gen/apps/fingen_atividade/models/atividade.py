caa1ade6f407d503e427e503fb3b18b21c39017b
Guilherme Brito <ghenriquegb9@gmail.com>
2021-10-07 19:43:14 -0300

    fingen_atividade: Field de data de vigência adicionado ao model Atividade

--- fin_gen/apps/fingen_atividade/models/atividade.py
+++ fin_gen/apps/fingen_atividade/models/atividade.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
 from fin_gen.base.models.base import Base
 
 
 class Atividade(Base):
-    titulo      = models.CharField('Título', max_length=128)
-    descricao   = models.CharField('Descrição', max_length=256)
-    valor       = models.DecimalField('Valor', max_digits=14, decimal_places=2)
-    moeda       = models.ForeignKey('fingen_financeiro.Moeda', on_delete=models.PROTECT)
-    categoria   = models.ForeignKey('fingen_atividade.Categoria', on_delete=models.PROTECT)
+    titulo              = models.CharField('Título', max_length=128)
+    descricao           = models.CharField('Descrição', max_length=256)
+    valor               = models.DecimalField('Valor', max_digits=14, decimal_places=2)
+    moeda               = models.ForeignKey('fingen_financeiro.Moeda', on_delete=models.PROTECT)
+    categoria           = models.ForeignKey('fingen_atividade.Categoria', on_delete=models.PROTECT)
+    data_da_atividade   = models.DateTimeField('Data de vigência', default=now)
