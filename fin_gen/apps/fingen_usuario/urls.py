5b421a44733ead68165fcca1ff52295392ba9b44
Guilherme Brito <ghenriquegb9@gmail.com>
2021-10-12 10:34:11 -0300

    fingen_usuario: Ajuste na view de login

--- fin_gen/apps/fingen_usuario/urls.py
+++ fin_gen/apps/fingen_usuario/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .forms import UsuarioAuthForm
 
 urlpatterns = [
-    path('login', LoginView.as_view(template_name='fingen_usuario/login.html', form_class=UsuarioAuthForm), name='login'),
+    path('login', LoginView.as_view(template_name='fingen_usuario/login.html', form_class=UsuarioAuthForm, redirect_authenticated_user=True), name='login'),
     path('logout', LogoutView.as_view(), name="logout")
 ]
