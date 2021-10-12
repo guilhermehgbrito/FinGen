650200cac975dc7ac1b8e86d46a1d079c4d25267
Guilherme Brito <ghenriquegb9@gmail.com>
2021-10-12 11:42:26 -0300

    fingen_usuario: Criada rota, view e template para registro de usuario

--- fin_gen/apps/fingen_usuario/urls.py
+++ fin_gen/apps/fingen_usuario/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from .forms import UsuarioAuthForm
+from .views import registro
 
 urlpatterns = [
     path('login', LoginView.as_view(template_name='fingen_usuario/login.html', form_class=UsuarioAuthForm, redirect_authenticated_user=True), name='login'),
-    path('logout', LogoutView.as_view(), name="logout")
+    path('logout', LogoutView.as_view(), name="logout"),
+    path('registro', registro, name="registro"),
 ]
