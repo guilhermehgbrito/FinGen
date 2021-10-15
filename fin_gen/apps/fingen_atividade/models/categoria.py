6f03efce6c3d567640dc9be7184c5c056f21b164
Guilherme Brito <ghenriquegb9@gmail.com>
2021-10-14 21:29:08 -0300

    fingen_atividade: Model e migrations para model atividade criados

--- fin_gen/apps/fingen_atividade/models/categoria.py
+++ fin_gen/apps/fingen_atividade/models/categoria.py
@@ -7,3 +7,6 @@ class Categoria(Base):
     titulo  = models.CharField('Título', max_length=128, unique=True)
     icone   = models.ForeignKey('fingen_atividade.Icone', on_delete=models.PROTECT)
     cor     = models.CharField('Cor', help_text='Valor em hexadecimal. Ex:#FFFFFF', max_length=7, validators=[MinLengthValidator(7)])
+
+    def __str__(self) -> str:
+        return self.titulo
