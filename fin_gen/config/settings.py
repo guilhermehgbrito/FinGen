d120637b26dd3e58086215a845e9f53453ce1100
Guilherme Brito <ghenriquegb9@gmail.com>
2021-10-21 20:37:58 -0300

    deploy: Ajuste db

--- fin_gen/config/settings.py
+++ fin_gen/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,14 +87,7 @@ WSGI_APPLICATION = 'fin_gen.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', cast=str),
-        'HOST': config('DB_HOST', cast=str),
-        'PORT': config('DB_PORT', cast=str),
-        'USER': config('DB_USER', cast=str),
-        'PASSWORD': config('DB_PASSWORD', cast=str),
-    }
+    'default': dj_database_url.config()
 }
 
 
