9fddc95c6a18c8be297bdc8a217f359665d081fe
Guilherme Brito <ghenriquegb9@gmail.com>
2021-11-03 20:25:18 -0300

    fingen_relatorio: Criados templates, views e rotas

--- fin_gen/apps/fingen_relatorio/apps.py
+++ fin_gen/apps/fingen_relatorio/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FingenRelatorioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fin_gen.apps.fingen_relatorio'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fin_gen.apps.fingen_relatorio"
